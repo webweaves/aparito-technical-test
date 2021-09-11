9df6dfeec7b593571719e0996a3a6dc070274a62
csw <christianweaves@gmail.com>
2021-09-11 08:58:54 +0100

    Work in progress

--- web-frontend/src/main/java/com/csw/aparito/FrontendController.java
+++ web-frontend/src/main/java/com/csw/aparito/FrontendController.java
@@ -1,17 +1,32 @@
 package com.csw.aparito;
 
 import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
 
 @Named
+@RequestScoped
 public class FrontendController {
 
+	/*
+	 * storage for GUI user value
+	 */
+	private Integer primeLimit;
+	
 	/**
 	 * todo csw
 	 * 
 	 * @return
 	 */
 	public String primeNumberClick() {
-		System.out.println("Clicked");
+		System.out.println("Supplied: " + getPrimeLimit());
 		return null;
+	}
+
+	public Integer getPrimeLimit() {
+		return primeLimit;
+	}
+
+	public void setPrimeLimit(Integer primeLimit) {
+		this.primeLimit = primeLimit;
 	}
 }
