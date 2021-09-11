0dfbb07a0f110dac9af5174ea559e9a1b52fcd65
csw <christianweaves@gmail.com>
2021-09-11 08:22:30 +0100

    Work in progress

---
+++ web-frontend/src/main/java/com/csw/aparito/FrontendController.java
@@ -0,0 +1,17 @@
+package com.csw.aparito;
+
+import javax.inject.Named;
+
+@Named
+public class FrontendController {
+
+	/**
+	 * todo csw
+	 * 
+	 * @return
+	 */
+	public String primeNumberClick() {
+		System.out.println("Clicked");
+		return null;
+	}
+}
