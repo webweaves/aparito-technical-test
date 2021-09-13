c7c906296a8bcef36c16fc7eb7d6097589b7a868
csw <christianweaves@gmail.com>
2021-09-13 18:01:15 +0100

    Fixed formatting

--- primenumbers-rest-api/src/test/java/com/csw/aparito/PrimeNumbersControllerTest.java
+++ primenumbers-rest-api/src/test/java/com/csw/aparito/PrimeNumbersControllerTest.java
@@ -24,5 +24,5 @@ public class PrimeNumbersControllerTest {
 		assertEquals("[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]", 
 				primeNumbersController.getPrimeNumbers(50).toString());
 		
-			}
+	}
 }
